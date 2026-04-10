461aa3fd2fb6801a157d6eab7d793ea2ba2a548b
malikyano <malikyano162@gmail.com>
2026-04-10 13:11:15 +0200

    Remove Tailwind CSS and path aliasing from config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-import path from 'path';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: './', // مهم جداً باش الروابط يمشوا صح
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './'),
-    },
-  },
-  build: {
-    outDir: '.', // لازم يخرج الملفات في الدوسي الرئيسي كيما درنا في Vercel
-    assetsDir: 'assets',
-    emptyOutDir: false, // باش ما يمسحش ملفاتك المهمة
-  }
+  plugins: [react()],
 });
