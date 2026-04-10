d7487b2548c0c694950ee6546a0abf26265dd2f3
malikyano <malikyano162@gmail.com>
2026-04-10 13:51:42 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // هادي هي اللي تخلي المتصفح يلقى الملفات في Vercel
   build: {
-    outDir: '.', // باش يخلي الملفات في الدوسي الرئيسي كيما درنا في Vercel
-    assetsDir: 'assets', // باش ينظم الصور والستيل
+    outDir: 'dist', // نرجعو للمجلد الافتراضي
   }
 });
