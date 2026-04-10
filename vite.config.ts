62c606db3361cc03fd64b57e82b5f0f642b2f48d
malikyano <malikyano162@gmail.com>
2026-04-10 13:55:53 +0200

    Clarify outDir comment in vite.config.ts
    Updated comment in build configuration for clarity.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // نرجعو للمجلد الافتراضي
+    outDir: 'dist', // باش يخدم المجلد اللي راهو يحوس عليه Vercel
   }
 });
