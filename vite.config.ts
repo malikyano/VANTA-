1bb904a9fce06cbdd5ad7c9c2965d6a13491f8e9
malikyano <malikyano162@gmail.com>
2026-04-10 13:44:22 +0200

    Update Vite configuration with base and build options
    Add base path and build configuration to Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // هادي هي اللي تنحي الشاشة البيضاء
+  build: {
+    outDir: '.',
+  }
 });
