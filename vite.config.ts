afa6ae411e912fa13af0d74992061388ea4a3035
malikyano <malikyano162@gmail.com>
2026-04-10 13:00:55 +0200

    Update Vite config with build settings
    Added build configuration for output directory and assets.

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,15 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './',
+  base: './', // مهم جداً باش الروابط يمشوا صح
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './'),
     },
   },
+  build: {
+    outDir: '.', // لازم يخرج الملفات في الدوسي الرئيسي كيما درنا في Vercel
+    assetsDir: 'assets',
+    emptyOutDir: false, // باش ما يمسحش ملفاتك المهمة
+  }
 });
